ad3695384348fc421c9b4c11632fadd740bc4df0
John R Iversen <john@johniversen.org>
2014-06-10 23:46:45 -0700

    best.R

--- best.R
+++ best.R
@@ -1,10 +1,23 @@
 ## best.R
 
 best <- function(state, outcome) {
+  
   ## Read outcome data
+  data <- read.csv("./ProgAssignment3-data/outcome-of-care-measures.csv", colClasses = "character",na.strings="Not Available")
   
   ## Check that state and outcome are valid
+  validOutcome = c("heart attack","heart failure","pneumonia")
+  if (!outcome %in% validOutcome) { stop("invalid outcome")}
+  
+  validState = unique(data[,7])
+  if (!state %in% validState) stop("invalid state")
+  
+  ## convert outcome name into column name
+  fullColName <- c("Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure", "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia")
+  colName <- fullColName[match(outcome,validOutcome)]
   
   ## Return hospital name in that state with lowest 30-day death rate
-  
+  data.state <- data[data$State==state,]
+  idx <- which.min(as.double(data.state[,colName]))
+  data.state[idx,"Hospital.Name"]
 }
