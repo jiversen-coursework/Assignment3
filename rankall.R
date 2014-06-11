e0433b06a86822e8f53858ca3fd6bfac36487a88
John R Iversen <john@johniversen.org>
2014-06-10 23:47:10 -0700

    rankall.R

--- rankall.R
+++ rankall.R
@@ -2,11 +2,37 @@
 
 rankall <- function(outcome, num = "best") {
   ## Read outcome data
+  data <- read.csv("./ProgAssignment3-data/outcome-of-care-measures.csv", colClasses = "character",na.strings="Not Available")
   
   ## Check that state and outcome are valid
+  validOutcome = c("heart attack","heart failure","pneumonia")
+  if (!outcome %in% validOutcome) { stop("invalid outcome")}
+  
+  validState = sort(unique(data[,7]))
+  if (!state %in% validState) stop("invalid state")
+  
+  ## convert outcome name into column name
+  fullColName <- c("Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure", "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia")
+  colName <- fullColName[match(outcome,validOutcome)]
   
   ## For each state, find the hospital of the given rank
+  hospital<-character(0)
+  
+  for (i in seq_along(validState)) {
+    ## Return hospital name in that state with the given rank 30-day death rate
+    data.state <- data[data$State==validState[i],]
+    
+    # order data by outcome
+    sorted.data.state <- data.state[order(as.numeric(data.state[[colName]]),data.state[["Hospital.Name"]],decreasing=FALSE,na.last=NA), ]
+    
+    #handle num input
+    this.num = num
+    if (this.num=="best") this.num = 1
+    if (this.num=='worst') this.num = nrow(sorted.data.state)
+    
+    hospital[i] <- sorted.data.state[this.num,"Hospital.Name"]
+  }
   
   ## Return a data frame with the hospital names and the (abbreviated) state name
-  
+  data.frame(hospital=hospital,state=validState,row.names=validState)
 }
